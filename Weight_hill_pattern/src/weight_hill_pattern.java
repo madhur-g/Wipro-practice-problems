8f1400eff6e233914288fe0b20bbf72665ffa395
gmadh <gmadh@DESKTOP-652IH32>
2020-06-16 15:02:37 +0530

    comments

--- Weight_hill_pattern/src/weight_hill_pattern.java
+++ Weight_hill_pattern/src/weight_hill_pattern.java
@@ -2,9 +2,9 @@
 public class weight_hill_pattern {
 
 	public static void main(String[] args) {
-		int x=4;
-		int y=1;
-		int z=5;
+		int x=4; //input1=x
+		int y=1; //input2=y
+		int z=5; //input3=z
 		int res=y;
 		int a=y+z;
 		int smp=0;
